78b9fc48f1f6389d2726c5374c914e375bf949fc
Bayardo Rivas <bayardo.rivas@gmail.com>
2022-05-21 13:51:58 -0600

    Creating file structure

--- js/pacmen.js
+++ js/pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -21,10 +21,10 @@ function makePac() {
   let position = setToRandom(200);
 
   // Add image to div id = game
-  let game = document.getElementById('game');
-  let newimg = document.createElement('img');
-  newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = "./images/PacMan1.png";
   newimg.width = 100;
 
   // TODO: set position here
@@ -56,10 +56,17 @@ function update() {
 
 function checkCollisions(item) {
   // TODO: detect collision with all walls and make pacman bounce
-  if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth || item.position.x + item.velocity.x < 0) {
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  ) {
     item.velocity.x = -item.velocity.x;
-  }  
-  if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight || item.position.y + item.velocity.y < 0) {
+  }
+  if (
+    item.position.y + item.velocity.y + item.newimg.height >
+      window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  ) {
     item.velocity.y = -item.velocity.y;
   }
 }
@@ -69,6 +76,6 @@ function makeOne() {
 }
 
 //don't change this line
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = { checkCollisions, update, pacMen };
 }
